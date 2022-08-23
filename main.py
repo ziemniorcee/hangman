ca1f1c255a931f3d6f8df33f8587534e7299704f
Igor <ziemniorcee@gmail.com>
2022-08-23 19:00:03 +0200

    xdf

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
-word = "anakonda"
-check = list("anakonda")
-result = "_" * len(word)
-result = list(result)
+def hangman():
+    word = "anakonda"
+    check = list("anakonda")
+    result = "_" * len(word)
+    result = list(result)
 
+    hang = 0
+    pics = ["  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
 
-hang = 0
-pics = ["  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========",
+            "  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=========",
 
-        "  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=========",
+            "  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n=========",
 
-        "  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n=========",
+            "  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=========",
 
-        "  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=========",
+            "  +---+\n  |   |\n  O   |\n /|\  |\n      |\n      |\n=========",
 
-        "  +---+\n  |   |\n  O   |\n /|\  |\n      |\n      |\n=========",
+            "  +---+\n  |   |\n  O   |\n /|\  |\n /    |\n      |\n=========",
 
-        "  +---+\n  |   |\n  O   |\n /|\  |\n /    |\n      |\n=========",
+            "  +---+\n  |   |\n  O   |\n /|\  |\n / \  |\n      |\n========="]
 
-        "  +---+\n  |   |\n  O   |\n /|\  |\n / \  |\n      |\n========="]
+    win = True
 
-win = True
+    while hang < 6 and win:
+        print(result)
+        b = 0
+        x = input("Podaj literke")
+        if x in word:
+            for let in word:
 
-while hang < 6 and win:
-    print(result)
-    b = 0
-    x = input("Podaj literke")
-    if x in word:
-        for let in word:
+                if x == let:
+                    result[b] = let
+                b = b + 1
+        else:
+            hang = hang + 1
+            print(pics[hang])
+        if result == check:
+            print("Wygrałeś!")
+            win = False
 
-            if x == let:
-                result[b] = let
-            b = b + 1
-    else:
-        hang = hang + 1
-        print(pics[hang])
-    if result == check:
-        print("Wygrałeś!")
-        win = False
-
-
+hangman()
